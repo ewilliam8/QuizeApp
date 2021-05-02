4370fd8ced71dbf0e3fbd76173f5d4eb5c1fe49e
ewilliam8 <rogachev.ilya@list.ru>
2021-05-03 00:02:32 +0700

    script

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const quize_data = [{
+        question: "What is the most popular music in the world?",
+        a: "Counting Stars",
+        b: "Shape of You",
+        c: "Despacito",
+        d: "See You Again",
+        correct: "c",
+    },
+    {
+        question: "What is the most used programming language in 2019?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "Who is the President of US?",
+        a: "Florin Pop",
+        b: "Donald Trump",
+        c: "Ivan Saldano",
+        d: "Mihai Andrei",
+        correct: "b",
+    },
+    {
+        question: "What is the most popular language in the world?",
+        a: "Chinese",
+        b: "Spanish",
+        c: "English",
+        d: "Hindi",
+        correct: "a",
+    },
+    {
+        question: "What is the most popular job in the world?",
+        a: "Public School Teacher",
+        b: "Engineering",
+        c: "Nurse Practitioner",
+        d: "Physical Therapist",
+        correct: "d",
+    }
+];
